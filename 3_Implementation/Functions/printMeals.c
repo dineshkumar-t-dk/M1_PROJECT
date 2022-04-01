d88fe4cdc6094c6a224e8bb821125a6a58aec97e
dineshkumar-t-dk <102506896+dineshkumar-t-dk@users.noreply.github.com>
2022-04-01 22:49:50 +0530

    Add files via upload

---
+++ 3_Implementation/Functions/printMeals.c
@@ -0,0 +1,16 @@
+#include"Restaurant_Bill.h"
+void printMeals()
+{
+      printf("\20******************  WELCOME TO OUR RESTURANT *************************\20\n");
+      printf(" \t\t\t BELOW IS THE MENU:\20\n");
+      printf(" \t\t\t ITEMS\t\t\tPRICE:\n");
+      printf(" \t\t\t \22*******************************\22\n");
+      printf(" \t\t\t 1- Fried Rice\t\tRupees 100.00\n");
+      printf(" \t\t\t 2- Biriyani(Chicken)\tRupees 150.00\n");
+      printf(" \t\t\t 3- Noodles\t\tRupees  60.00\n");
+      printf(" \t\t\t 4- Chicken 65\t\tRupees  85.00\n");
+      printf(" \t\t\t 5- Scoop icecream\tRupees  30.00\n");
+      printf(" \t\t\t 6- Cone ice\t\tRupees  15.00\n");
+      printf(" \t\t\t 7- Tandoori\t\tRupees 200.00\n");
+      printf("\n");
+}
